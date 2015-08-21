6d29956d10d352fe1f9728437e80007002781aa7
keving <kevin.green@workshare.com>
2015-08-21 11:42:52 +0100

    add daemon and file tests per the docs.

--- chef-repo/sshd.rb
+++ chef-repo/sshd.rb
@@ -1,6 +1,21 @@
+# This should be conditional on OS being linux at very least!
+
 package 'openssh-server'
 
 service 'ssh' do
    supports :status => true
    action [:enable, :start]
+end
+
+file '/home/keving/test.txt' do
+   content 'This is
+a multi
+line
+test
+of
+chef
+b0rk
+b0rk
+ping!
+'
 end
