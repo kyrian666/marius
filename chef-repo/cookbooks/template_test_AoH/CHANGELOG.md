5f9891827f4f6c064e6c97bcf9bd722c830c7d63
Kev Green <kevin.green@workshare.com>
2015-08-28 15:32:56 +0100

    so this actually works then, finally, after breaking our brains!!

---
+++ chef-repo/cookbooks/template_test_AoH/CHANGELOG.md
@@ -0,0 +1,13 @@
+template_test_AoH CHANGELOG
+===========================
+
+This file is used to list changes made in each version of the template_test_AoH cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of template_test_AoH
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ chef-repo/cookbooks/template_test_AoH/recipes/default.rb
@@ -0,0 +1,45 @@
+
+class Chef::Recipe
+  include Test
+end
+
+# Cookbook Name:: template_test_AoH
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+	node.run_state[:test_list] = test_list
+
+        node.run_state[:test_list].each do |mymicro|
+            template "/tmp/ms-#{mymicro[:api_name]}-balanced.conf" do
+                source "microservices_balanced.conf.erb"
+                owner "root"
+                group "root"
+                mode 0644
+                variables ({
+                    'api_name' => mymicro[:api_name], 
+                    'api_uri' => mymicro[:api_uri],
+                    'api_port_back' => mymicro[:api_port_back],
+                    'api_hosts_back' => mymicro[:api_hosts_back]
+                })
+            end
+        end
+
+#	log node.run_state[:test_list][0] do 
+#            level :warn
+#        end
+
+	p node.run_state[:test_list]
+
+        template '/tmp/proxy.conf' do
+            source "test.erb"
+            owner "root"
+            group "root"
+            mode 0644
+            variables(
+		{ 'ms_list'=>node.run_state[:test_list] }
+	    )
+      end
+
